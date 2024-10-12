1510335b1a85e4d93f21c75e26c00f4eb821335b
Guide <thammatorn.sud@gmail.com>
2024-10-12 23:06:05 +0700

    test img

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // ใช้ Routes แทน Switch
-import StarTriangle from './component/StarTriangle';
-import PokemonDex from './component/PokemonDex';
-import Resume from './component/Resume'; 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom"; // ใช้ Routes แทน Switch
+import StarTriangle from "./component/StarTriangle";
+import PokemonDex from "./component/PokemonDex";
+import Resume from "./component/Resume";
 import Navbar from "./component/Navbar";
+import image1 from "./image/my-image.png";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/StarTriangle" element={<StarTriangle />} /> {/* ใช้ element เป็น JSX */}
-          <Route path="/pokemondex" element={<PokemonDex />} />
-          <Route path="/Resume" element={<Resume />} />
-          <Route path="/StarTriangle" element={<StarTriangle />} /> {/* จัดให้ StarTriangle แสดงที่หน้าแรก */}
-          <Route path="/" element={<Navigate to="StarTriangle" />} />
-          <Route path="*" element={<StarTriangle />} /> {/* This will show the NotFound component */}
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <div>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/StarTriangle" element={<StarTriangle />} /> {/* ใช้ element เป็น JSX */}
+    //       <Route path="/pokemondex" element={<PokemonDex />} />
+    //       <Route path="/Resume" element={<Resume />} />
+    //       <Route path="/StarTriangle" element={<StarTriangle />} /> {/* จัดให้ StarTriangle แสดงที่หน้าแรก */}
+    //       <Route path="/" element={<Navigate to="StarTriangle" />} />
+    //       <Route path="*" element={<StarTriangle />} /> {/* This will show the NotFound component */}
+    //     </Routes>
+    //   </div>
+    // </Router>
+    <div>
+      image
+      <img src={image1} alt="" />
+    </div>
   );
 }
